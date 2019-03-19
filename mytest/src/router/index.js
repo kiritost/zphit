8de87ba46006b1654b1d53e70f5864b6de18a80f
kiritost <1240547857@qq.com>
2019-03-19 13:46:01 +0800

    Add files via upload

--- mytest/src/router/index.js
+++ mytest/src/router/index.js
@@ -1,1 +1,12 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import foods from '@/components/foods.vue'
+import ratings from '@/components/rating.vue'
+import seller from '@/components/seller.vue'
+Vue.use(Router)
 
+let routes = [
+  {path: '/foods', component: foods}, {path: '/ratings', component: ratings}, {path: '/seller', component: seller}
+]
+
+export default routes
