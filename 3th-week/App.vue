529a941b79077d303aa388799f089e29e92243b0
kiritost <1240547857@qq.com>
2019-03-19 17:04:23 +0800

    Add files via upload

---
+++ 3th-week/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <!--
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    -->
+    {{ info }}
+  </div>
+</template>
+
+<script>
+//import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      info: null
+    }
+  },
+  mounted () {
+    this.$axios
+      .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+      .then(response => (this.info = response))
+      .catch(error => console.log(error))
+  },
+  components: {
+//    HelloWorld
+  }
+}
+</script>
--- 3th-week/main.js
+++ 3th-week/main.js
@@ -1,1 +1,13 @@
+import Vue from 'vue'
+import App from './App.vue'
+import Axios from 'axios'
+import VueAxios from 'vue-axios'
 
+Vue.use(VueAxios, Axios)
+
+Vue.config.productionTip = false
+Vue.prototype.$axios = Axios
+
+new Vue({
+  render: h => h(App)
+}).$mount('#app')
